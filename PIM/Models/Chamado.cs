98875017bd48e60082ddc5aafa53595ed7386b4c
Welison-Kaw <welisoncomw@gmail.com>
2018-11-13 01:37:22 -0200

    Commit de atualizaçao

---
+++ PIM/Models/Chamado.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PIM.Models
+{
+    public class Chamado
+    {
+        public int id { get; set; }
+        public int Departamento { get; set; }
+        public int GrauUrgencia { get; set; }
+        public string Andar { get; set; }
+        public string Equipmaento { get; set; }
+        public string Descricao { get; set; }
+        public int Cliente { get; set; }
+        public int Funcionario { get; set; }
+    }
+}
