18202b0b7b8ddd2003ab77622f09331ba8619e21
Welison-Kaw <welisoncomw@gmail.com>
2018-11-20 18:48:49 -0200

    Propriedades das classes

--- PIM/Models/Objects/Cliente.cs
+++ PIM/Models/Objects/Cliente.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +8,13 @@ namespace PIM.Models.Objects
 {
     public class Cliente
     {
+        [Display(Name = "Código")]
+        public int id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public int DepartamentoID { get; set; }
+
+        public virtual Departamento Departamento { get; set; }
     }
 }
