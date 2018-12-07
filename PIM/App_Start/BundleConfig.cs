70e2aa1f522f0bf38242488750661e6f1924f3a1
Welison-Kaw <welisoncomw@gmail.com>
2018-12-07 00:59:21 -0200

    Atualizaçao de html das views

--- PIM/App_Start/BundleConfig.cs
+++ PIM/App_Start/BundleConfig.cs
@@ -21,10 +21,13 @@ namespace PIM
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
+                      "~/Scripts/solid.js",
+                      "~/Scripts/fontawesome.js",
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/navbar.css",
                       "~/Content/site.css"));
         }
     }
